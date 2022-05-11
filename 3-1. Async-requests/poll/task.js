135889fe36eba49369a4164ee4d13005cce49149
Gioza <karagiozov.a@mail.ru>
2022-05-11 14:27:14 +0300

    In process

---
+++ 3-1. Async-requests/poll/task.js
@@ -0,0 +1,31 @@
+
+const xhr = new XMLHttpRequest();
+xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+        let resp = JSON.parse(xhr.response);
+
+        const title = document.getElementById('poll__title');
+        title.textContent = resp.data.title;
+        const serverResp = resp.data.answers;
+        const answer = document.querySelectorAll('.poll__answer');
+        answer.forEach(item => {
+            item.addEventListener('click', () => {
+                alert('Спасибо, ваш голос засчитан!');
+            })
+        })
+
+        const answersList = document.getElementById('poll__answers');
+        console.log(answersList)
+
+        serverResp.forEach((button) => {
+            const item = document.createElement('button');
+            item.classList.add('poll__answer');
+            item.textContent = `${button}`;
+            console.log(item)
+        })
+    }
+});
+
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhr.send();
+
